f31eb4e3aac4fd5f0eed47fe0a37b4655ff6990e
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2024-01-14 18:28:40 +0100

    cleaner code

--- configuration.nix
+++ configuration.nix
@@ -99,10 +99,6 @@
     description = "Roberto";
     shell = pkgs.fish;
     extraGroups = [ "networkmanager" "wheel" "dialout" "video" "i2c" ];
-    packages = with pkgs; [
-      firefox
-      vscode-fhs
-    ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1zKtf72PNwwrYhc5StUy2UQeWGwCBiGHRQK/gpTLCqNleloK87q33628BGVx+f9oU2YcvF5+rwLeRJZEUpo3cdkHK5vLV9fM4T7pPWQBAkvNm0U3tro+AiODCw3XQEn0lbjLsw3MjhR7oZCHFbFRzJ77xQKMdr6O+qyamULLOgpEzmdJxA60dWsMiShDKX/2k31fmpAweP48UDqfUfgXwnk+93gfaKh+4kGCmMOCHICLi9nTK53GocLgPeBB8Hq8V6fdxdH3QzCUMQ/FOiclcvXeDmIYaN8DFR1fIJeGCIl7zdLpvwEC9013Gmtgb3Pr3hzkFECG17+3LHydlhHx+5EDfx2C0IW8l9veJXOREw1Qt9FtAbLnCqbe+hcq0apOL+Qdgtu4Yfis8VSt0JjSr3WnGNLGi7D3+Q99VQ1cgZ4lP0oRQ+6A4fXWpRjq2PZPBEcRwqbqMjQdK5zciLkfvXPiJiV+QuaszyGA7pDgst0Ve81wVsfHd2sx8Vqzf4T8SzeUwlk3o/I/jZCPOYkshENl8+R4stDf96sHD9whqXuxRI+GWqa24F+RuCHHUdMKNblk7iT7Ap9NacXfJf/8j2QXa2eJGtN1m4/yHn+tINkC8ubuS9jN/DzbVYdqFSo2s8ShftLTFyRKRkhKzqrKl6En6Y/AsASaMzqr1m1yB7w== roblor"
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJ4bzLMIpw2WK3DYhJENru2f5UEMHENnHRLpf6U8sxqkYC8m2TAN0CO9uTsDdAJuEtwETIRLkyx3B5zhdwow0fA="
@@ -261,4 +257,6 @@
     enable = true;
     resyncTimer = "1h";
   };
+
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }

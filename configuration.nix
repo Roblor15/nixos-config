3559c7569cfa275915060274589d08e4f07dd251
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2024-11-12 17:54:40 +0200

    update zen and add nvidia to cachix

--- configuration.nix
+++ configuration.nix
@@ -15,11 +15,13 @@
       "https://hyprland.cachix.org"
       "https://anyrun.cachix.org"
       "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
 

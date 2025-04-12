c29bc598f0bf0db10fc51c1808a4164d38827ea3
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2025-04-12 20:06:56 +0200

    changes for non-interructive shells

--- programs/bash.nix
+++ programs/bash.nix
@@ -6,14 +6,10 @@
     enableCompletion = true;
     bashrcExtra = ''
       export PATH=$PATH:~/.cargo/bin
-
-      eval "$(starship init bash)"
       eval "$(rustic completions bash 2> /dev/null)" 
-
-      alias backup-data="borg create --stats --progress --compression auto,lzma,9 --patterns-from ~/borg-dati-patterns.lst /run/media/roblor/Roblor\'s\ Files/borg-backup::Dati-{now:%Y-%m-%dT%H:%M:%S}"
-      alias backup-home="borg create --stats --progress --compression auto,lzma,9 --patterns-from ~/borg-home-patterns.lst /run/media/roblor/Roblor\'s\ Files/borg-backup::Home-{now:%Y-%m-%dT%H:%M:%S}"
-      alias esp-idf="nix develop github:mirrexagon/nixpkgs-esp-dev#esp32-idf"
       alias clean-generations="sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +2"
+      if [[ $- == *i* ]]; then
+        eval "$(starship init bash)"
     '';
   };
 }
--- programs/fish.nix
+++ programs/fish.nix
@@ -6,10 +6,10 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       nix-your-shell fish | source
+      tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Round --powerline_prompt_tails=Slanted --powerline_prompt_style='Two lines, frame' --prompt_connection=Solid --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Sparse --icons='Few icons' --transient=No
     '';
     shellInit = ''
       zoxide init fish | source
-      tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Round --powerline_prompt_tails=Slanted --powerline_prompt_style='Two lines, frame' --prompt_connection=Solid --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Sparse --icons='Few icons' --transient=No
     '';
     plugins = [
       {

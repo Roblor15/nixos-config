d09404a02126a502eb8f0420fdae69df67c0ced0
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2025-04-27 17:10:26 +0200

    no autosuspend with gdm on desktop

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = (variants.hostName != "roblor-desktop");
   services.xserver.desktopManager.gnome.enable = variants.gnome;
 
   # Configure keymap in X11
